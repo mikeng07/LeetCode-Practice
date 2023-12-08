a0b2ac24086e392847ab901fd747dcd40abea87f
Mike <nguyentung2107@gmail.com>
2023-12-08 14:59:56 -0800

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    i , j = 0, len(numbers) -1
+    while (i<j):
+        if (numbers[i] + numbers[j]) < target:
+            i += 1
+        elif (numbers[i] + numbers[j]) > target:
+            j += 1
+        else:
+            return i + 1, j + 1
+        
+
+numbers = [2,7,11,15]
+ans = twoSum(numbers)
+
+print(ans)

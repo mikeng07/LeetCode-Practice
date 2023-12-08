b8d09f0a8a1d101dffd8d3903b597c2355dfb73a
Mike <nguyentung2107@gmail.com>
2023-12-08 15:11:52 -0800

    answer

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from typing import List
 
-
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
+def twoSum( numbers: List[int], target: int) -> List[int]:
     i , j = 0, len(numbers) -1
     while (i<j):
         if (numbers[i] + numbers[j]) < target:
             i += 1
         elif (numbers[i] + numbers[j]) > target:
-            j += 1
+            j -= 1
         else:
-            return i + 1, j + 1
+            return [i + 1, j + 1]
         
 
 numbers = [2,7,11,15]
-ans = twoSum(numbers)
+target = 9
+ans = twoSum(numbers,target)
 
 print(ans)
